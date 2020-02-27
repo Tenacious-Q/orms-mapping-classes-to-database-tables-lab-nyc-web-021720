aaffb030fa69bd9bce26f4285fbeef708cc2b67c
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-27 22:10:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,10 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
 
-  initialize(name, grade, id=nil)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
