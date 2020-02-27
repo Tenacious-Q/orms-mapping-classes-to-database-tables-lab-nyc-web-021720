ca3b4eb01ee1cd28624a54d0ee6849fe7d702496
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-27 22:27:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,6 +15,15 @@ class Student
 
   def self.create_table
 
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
   end
 
 
